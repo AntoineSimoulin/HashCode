246c53b1af3dd0aa16364c94c5ba47b61be728c1
Raphael <raphael.meudec@gmail.com>
2018-03-01 19:09:17 +0100

    parser

--- src/inout/instance.py
+++ src/inout/instance.py
@@ -1,11 +1,5 @@
 from collections import namedtuple
-# import sys
-# import numpy as np
-
-Endpoint = namedtuple('Endpoint', ['id', 'lat', 'con'])
-Request = namedtuple('Request', ['vid', 'eid', 'n'])
-Scoring = namedtuple('Scoring', ['vid', 'cid', 'score'])
-
+Ride = namedtuple('Ride', ['id', 'a', 'b', 'x', 'y', 's', 'f'])
 
 class Instance:
     def __init__(self, fpath, name='instance'):
@@ -14,39 +8,24 @@ class Instance:
         self.name = name
 
     def read_dataset(self):
-        with open(self.fpath, 'r') as f:
+        with open(self.fpath, 'r') as dataset_file:
 
-            # numbers
-            self.n_vid, self.n_end, self.n_req, self.n_cache, self.s_cache = \
-                [int(i) for i in f.readline().split(" ")]
+            self.R, self.C, self.F, self.N, self.B, self.T = [int(i) for i in dataset_file.readline().split(' ')]
+            self.rides = []
+            for i in range(self.N):
+                a, b, x, y, s, f = [int(i) for i in dataset_file.readline().split(' ')]
+                self.rides.append(Ride(i, a, b, x, y, s, f))
 
-            # video sizes
-            self.s_videos = [int(i) for i in f.readline().split(" ")]
 
-            # endpoints
-            self.endpoints = []
-            for e in range(self.n_end):
-                L_D, K = [int(i) for i in f.readline().split(" ")]
-
-                connections = []
-
-                # iterate over the K connected caches
-                for k in range(K):
-                    c, L_C = [int(i) for i in f.readline().split(" ")]
-                    connections.append((c, L_C))
-
-                self.endpoints.append(Endpoint(e, L_D, connections))
-
-            # requests
-            self.requests = []
-            for r in range(self.n_req):
-                R_v, R_e, R_n = [int(i) for i in f.readline().split(" ")]
-                self.requests.append(Request(R_v, R_e, R_n))
-
-            # return n_vid, n_end, n_req, n_cache, s_cache, s_videos, endpoints, requests
-
-    # def __repr__(self):
-    #     return "n_vid : %i" %self.n_vid
-
-    def __str__(self):
-        return "Instance name : %s \nn_vid : %i\nn_end : %i\nn_req : %i\nn_cache : %i\ns_cache : %i" %(self.name, self.n_vid, self.n_end, self.n_req, self.n_cache, self.s_cache)
+if __name__ == "__main__":
+    file_path = "../../data/c_no_hurry.in"
+    dataset = Instance(file_path)
+    print(dataset.R)
+    print(dataset.C)
+    print(dataset.F)
+    print(dataset.N)
+    print(dataset.B)
+    print(dataset.T)
+    print(dataset.rides)
+    for ride in dataset.rides:
+        print(ride.id, ' - ', ride.a, ride.b, ride.x, ride.y, ride.s, ride.f)
